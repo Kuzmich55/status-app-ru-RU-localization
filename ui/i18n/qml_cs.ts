c6062acdb516ce2769ea8905f9fbda6914e0df88
Michał Cieślak <michalcieslak@status.im>
2026-07-16 16:57:14 +0200

    Chat: encapsulate (edited) marker rendering in ChatTextView and ChatSingleLineTextView

--- ui/i18n/qml_cs.ts
+++ ui/i18n/qml_cs.ts
@@ -16657,6 +16657,13 @@ selhalo</translation>
     </message>
 </context>
 <context>
+    <name>StringUtils</name>
+    <message>
+        <source>(edited)</source>
+        <translation type="unfinished">(upraveno)</translation>
+    </message>
+</context>
+<context>
     <name>SupportedTokenListsPanel</name>
     <message numerus="yes">
         <source>%n token(s) · Last updated %1</source>
--- ui/i18n/qml_es.ts
+++ ui/i18n/qml_es.ts
@@ -16582,6 +16582,13 @@ al cargar</translation>
     </message>
 </context>
 <context>
+    <name>StringUtils</name>
+    <message>
+        <source>(edited)</source>
+        <translation type="unfinished">(editado)</translation>
+    </message>
+</context>
+<context>
     <name>SupportedTokenListsPanel</name>
     <message numerus="yes">
         <source>%n token(s) · Last updated %1</source>
