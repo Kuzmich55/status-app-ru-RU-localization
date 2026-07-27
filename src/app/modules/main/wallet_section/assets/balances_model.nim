status-im/status-app

perf(wallet): granular token model updates
Diff token groups and grouped account assets against a cached snapshot via
model_sync and emit granular insert/remove/dataChanged instead of
beginResetModel, with a self-updating nested BalancesModel and a stable by-key
source order to avoid spurious reorders. Market details are re-keyed by group
key so surviving groups keep their MarketDetailsItem across the diff.

src/app/modules/main/wallet_section/assets/balances_model.nim
@@ -1,7 +1,9 @@
 import app/modules/shared_models/model_utils
-import nimqml, tables, strutils, sequtils, stint
+import nimqml, tables, strutils, stint
 
-import ./io_interface
+import app_service/service/wallet_account/dto/asset_group_item
+import app/modules/shared/model_sync
+
 type
   ModelRole {.pure.} = enum
     Account = UserRole + 1,
@@ -14,21 +16,23 @@ type
 
 QtObject:
   type BalancesModel* = ref object of QAbstractListModel
-    delegate: io_interface.GroupedAccountAssetsDataSource
-    index: int
+    # Self-updating snapshot of the per-(token, account) balances of a single token
+    # group. Held by key (the parent keeps a BalancesModel per group key), NOT by
+    # positional index, so it survives the parent's granular reorders. The parent
+    # pushes fresh balances via updateBalances(), which diffs against this snapshot
+    # and emits granular dataChanged/insert/remove — this is the only path that
+    # delivers balance changes to the view without the parent resetting.
+    items: seq[BalanceItem]
 
   proc setup(self: BalancesModel)
   proc delete(self: BalancesModel)
-  proc newBalancesModel*(delegate: io_interface.GroupedAccountAssetsDataSource, index: int): BalancesModel =
+  proc newBalancesModel*(balances: seq[BalanceItem] = @[]): BalancesModel =
     new(result, delete)
     result.setup
-    result.delegate = delegate
-    result.index = index
+    result.items = balances
 
   method rowCount(self: BalancesModel, index: QModelIndex = nil): int =
-    if self.index < 0 or self.index >= self.delegate.getGroupedAssetsList().len:
-      return 0
-    return self.delegate.getGroupedAssetsList()[self.index].balancesPerAccount.len
+    return self.items.len
 
   proc countChanged(self: BalancesModel) {.signal.}
   proc getCount(self: BalancesModel): int {.slot.} =
@@ -51,7 +55,7 @@ QtObject:
   method data(self: BalancesModel, index: QModelIndex, role: int): QVariant =
     guardModelData(index, self.rowCount(), role, ModelRole)
 
-    let item = self.delegate.getGroupedAssetsList()[self.index].balancesPerAccount[index.row]
+    let item = self.items[index.row]
 
     let enumRole = role.ModelRole
     case enumRole:
@@ -70,8 +74,37 @@ QtObject:
       of ModelRole.Loading:
         result = newQVariant(item.loading)
 
+  proc balanceId(item: BalanceItem): string =
+    # A balance row is uniquely a (token, account) pair (tokenKey already encodes
+    # the chain). Stable across refreshes -> lets the diff recognise survivors.
+    item.tokenKey & "|" & item.account
+
+  proc balanceRolesChanged(oldItem, newItem: BalanceItem): seq[int] =
+    # Identity fields (account/tokenKey/chainId/tokenAddress/groupKey) are static for
+    # a given id; only the fetched value and its loading flag move.
+    result = @[]
+    if oldItem.balance != newItem.balance: result.add(ModelRole.Balance.int)
+    if oldItem.loading != newItem.loading: result.add(ModelRole.Loading.int)
+
+  proc updateBalances*(self: BalancesModel, newBalances: seq[BalanceItem]) =
+    # Granular in-place update: diff the new balances against the cached snapshot and
+    # emit only the minimal insert/remove/dataChanged. No beginResetModel -> the
+    # nested filteredBalances SFPM and the FunctionAggregators in AssetsViewAdaptor
+    # update in place instead of being rebuilt.
+    setItemsWithSync(
+      self, self.items, newBalances,
+      getId = proc(item: BalanceItem): string = balanceId(item),
+      getRoles = proc(oldItem, newItem: BalanceItem): seq[int] = balanceRolesChanged(oldItem, newItem),
+      countChanged = proc() = self.countChanged())
+
   proc setup(self: BalancesModel) =
     self.QAbstractListModel.setup
 
   proc delete(self: BalancesModel) =
     self.QAbstractListModel.delete
+
+  # Test-only accessors (used by grouped_account_assets_model_test).
+  proc balanceCountForTest*(self: BalancesModel): int =
+    self.items.len
+  proc balanceAtForTest*(self: BalancesModel, i: int): string =
+    self.items[i].balance.toString(10)

Kuzmich55/status-app-ru-RU-localization

src/app/modules/main/wallet_section/assets/balances_model.nim
@@ -1,7 +1,9 @@
 import app/modules/shared_models/model_utils
-import nimqml, tables, strutils, sequtils, stint
+import nimqml, tables, strutils, stint
 
-import ./io_interface
+import app_service/service/wallet_account/dto/asset_group_item
+import app/modules/shared/model_sync
+
 type
   ModelRole {.pure.} = enum
     Account = UserRole + 1,
@@ -14,21 +16,23 @@ type
 
 QtObject:
   type BalancesModel* = ref object of QAbstractListModel
-    delegate: io_interface.GroupedAccountAssetsDataSource
-    index: int
+    # Self-updating snapshot of the per-(token, account) balances of a single token
+    # group. Held by key (the parent keeps a BalancesModel per group key), NOT by
+    # positional index, so it survives the parent's granular reorders. The parent
+    # pushes fresh balances via updateBalances(), which diffs against this snapshot
+    # and emits granular dataChanged/insert/remove — this is the only path that
+    # delivers balance changes to the view without the parent resetting.
+    items: seq[BalanceItem]
 
   proc setup(self: BalancesModel)
   proc delete(self: BalancesModel)
-  proc newBalancesModel*(delegate: io_interface.GroupedAccountAssetsDataSource, index: int): BalancesModel =
+  proc newBalancesModel*(balances: seq[BalanceItem] = @[]): BalancesModel =
     new(result, delete)
     result.setup
-    result.delegate = delegate
-    result.index = index
+    result.items = balances
 
   method rowCount(self: BalancesModel, index: QModelIndex = nil): int =
-    if self.index < 0 or self.index >= self.delegate.getGroupedAssetsList().len:
-      return 0
-    return self.delegate.getGroupedAssetsList()[self.index].balancesPerAccount.len
+    return self.items.len
 
   proc countChanged(self: BalancesModel) {.signal.}
   proc getCount(self: BalancesModel): int {.slot.} =
@@ -51,7 +55,7 @@ QtObject:
   method data(self: BalancesModel, index: QModelIndex, role: int): QVariant =
     guardModelData(index, self.rowCount(), role, ModelRole)
 
-    let item = self.delegate.getGroupedAssetsList()[self.index].balancesPerAccount[index.row]
+    let item = self.items[index.row]
 
     let enumRole = role.ModelRole
     case enumRole:
@@ -70,8 +74,33 @@ QtObject:
       of ModelRole.Loading:
         result = newQVariant(item.loading)
 
+  proc syncKey(it: BalanceItem): string =
+    # A balance row is uniquely a (token, account) pair (tokenKey already encodes
+    # the chain). Stable across refreshes -> lets the diff recognise survivors.
+    it.tokenKey & "|" & it.account
+
+  proc syncRoles(o, n: BalanceItem): seq[int] =
+    # Identity fields (account/tokenKey/chainId/tokenAddress/groupKey) are static for
+    # a given id; only the fetched value and its loading flag move.
+    result = @[]
+    if o.balance != n.balance: result.add(ModelRole.Balance.int)
+    if o.loading != n.loading: result.add(ModelRole.Loading.int)
+
+  proc updateBalances*(self: BalancesModel, newBalances: seq[BalanceItem]) =
+    # Granular in-place update: diff the new balances against the cached snapshot and
+    # emit only the minimal insert/remove/dataChanged. No beginResetModel -> the
+    # nested filteredBalances SFPM and the FunctionAggregators in AssetsViewAdaptor
+    # update in place instead of being rebuilt.
+    self.modelSync(self.items, newBalances)
+
   proc setup(self: BalancesModel) =
     self.QAbstractListModel.setup
 
   proc delete(self: BalancesModel) =
     self.QAbstractListModel.delete
+
+  # Test-only accessors (used by grouped_account_assets_model_test).
+  proc balanceCountForTest*(self: BalancesModel): int =
+    self.items.len
+  proc balanceAtForTest*(self: BalancesModel, i: int): string =
+    self.items[i].balance.toString(10)